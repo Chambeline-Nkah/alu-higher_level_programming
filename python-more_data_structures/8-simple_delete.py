bab12bfc7f48f2d485d5da0fd6e0bf7a93f293d4
Chambeline-Nkah <chambelinenkah57@gmail.com>
2022-11-03 20:42:17 +0100

    273

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-     new_dict = dict(a_dictionary)
-    for key, value in new_dict.items():
-        new_dict[key] = value * 2
-    return new_dict
+       if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
