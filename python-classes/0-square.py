9ccf801a03618b1dcb86e83adfd145ed369f7362
Chambeline-Nkah <chambelinenkah57@gmail.com>
2022-11-10 22:15:42 +0200

    More about classes in python

---
+++ python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+'''Creating an Empty class Square'''
+
+class Square:
+    '''Square class'''
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Creating an Empty class Square'''
+
+class Square:
+    '''Square class with an attribute'''
+    def __init__(self, size):
+        '''Instance attribute'''
+        self.__size = size
