855eb0eb83363df44ee433a94169a127ccd6add9
Chambeline-Nkah <chambelinenkah57@gmail.com>
2022-11-10 23:45:19 +0200

    Removing the error

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -23,7 +23,7 @@ class Square:
     def area(self):
         '''Returns the area of a square'''
         return self.__size * self.__size
-    
+
     def my_print(self):
         if self.__size == 0:
             print()
