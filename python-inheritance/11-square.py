d729521bea4b8cf964b13ffb56b5c7fa2d19033b
Chambeline-Nkah <chambelinenkah57@gmail.com>
2022-11-17 06:46:31 +0200

    Corrections

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -6,6 +6,7 @@ Rectangle = __import__('9-rectangle.py').Rectangle
 
 class Square(Rectangle):
     '''Creating a class square that inherits from Rectangle'''
+
     def __init__(self, size):
         '''Defining the init function'''
         self.integer_validator('size', size)
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,9 +5,10 @@
 BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
 
 class Rectangle(BaseGeometry):
+
     '''Creating a class that inherits properties from BaseGeometry'''
 
-    
+
     def __init__(self, width, height):
         '''Function that takes in width and height'''
         self.integer_validator('width', width)
