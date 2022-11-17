00c56bcf223683f95596ffad550e9f37feb409da
Chambeline-Nkah <chambelinenkah57@gmail.com>
2022-11-17 20:01:39 +0200

    r & w files

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Returning the JSON representation of an object'''
+
+
+import json
+def to_json_string(my_obj):
+    '''Function that returns the JSON of an object'''
+    return json.dumps(my_obj)
