2073d2ca4a3f7678f47859b2d66ae60614f33b4d
Chambeline-Nkah <chambelinenkah57@gmail.com>
2022-11-22 18:10:10 +0200

    Corrections

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -9,10 +9,10 @@ import urllib.request
 import urllib.parse
 
 if __name__ == '__main__':
-    
+
     url = sys.argv[1]
     mssg = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(mssg).encode('ascii')
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
-       print(response.read().decode("utf-8"))
+        print(response.read().decode("utf-8"))
