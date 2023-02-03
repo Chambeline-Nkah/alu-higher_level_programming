4048e3f366774cf13015fec4c462e5ab7096da44
Ordunharlaryor <jegedeo172@gmail.com>
2023-02-03 08:52:56 +0200

    ##Indent

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-const args=process.argv.length;
-switch (args){
-    case 2:
-        console.log('No arguments');
-        break;
-    case 3:
-        console.log('Argument found');
-        break;
-    default:
-        console.log('Arguments found');
-        break;
+const args = process.argv[2];
+switch (args) {
+  case 0:
+    console.log('No arguments');
+    break;
+  case 1:
+    console.log('Argument found');
+    break;
+  default:
+    console.log('Arguments found');
+    break;
 }
