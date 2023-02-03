84be8f05277015d466a42119833778849462a8d6
Ordunharlaryor <jegedeo172@gmail.com>
2023-02-03 16:18:27 +0200

    6&7

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-for (let i=1; i<=3; i++) {
-  if (i==1) {
-    console.log('C is fun');
-  }
-  else if (i==2) {
-    console.log('Python is cool');
-  }
-  else {
-    console.log('JavaScript is amazing');
-  }
+const lang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < lang.length; i++) {
+  console.log(lang[i]);
 }
