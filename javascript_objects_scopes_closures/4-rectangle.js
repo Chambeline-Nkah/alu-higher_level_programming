d66d9b75a6b1ad5fbdefbfd65f53f4361456eda7
Ordunharlaryor <jegedeo172@gmail.com>
2023-02-06 08:50:29 +0200

    ##4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -6,20 +6,20 @@ class Rectangle {
     this.width = w;
     this.height = h;
   }
-  
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  rotate() {
+  rotate () {
     const rotwid = this.width;
-    this.width = this.width;
+    this.width = this.height;
     this.height = rotwid;
   }
 
-  double() {
+  double () {
     this.width = this.width * 2;
     this.height = this.height * 2;
   }
