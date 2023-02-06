584bbab49c1aa14c193fac8b94a270175327b66d
Ordunharlaryor <jegedeo172@gmail.com>
2023-02-06 14:07:28 +0200

    Space

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//Writing a script that display the status code of a GET request.
+// Writing a script that display the status code of a GET request.
 const request = require('request');
 
 request.get(process.argv[2], (error, response, body) => {
